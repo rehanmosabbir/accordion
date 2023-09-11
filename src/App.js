01b7816e53c1636eb84401b222dc7d0a1c24e50a
Md. Mosabbir Hossain <rehanmosabbir@gmail.com>
2023-09-11 11:43:01 +0600

    add index.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
+
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
 
 export default function App() {
   return (
@@ -10,5 +24,5 @@ export default function App() {
 }
 
 function Accordion() {
-  <p>TODO</p>;
+  return <p>TODO</p>;
 }
