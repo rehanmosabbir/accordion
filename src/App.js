4ca343d204a925e1206542de117581ec26d8cf9f
Md. Mosabbir Hossain <rehanmosabbir@gmail.com>
2023-09-11 16:42:03 +0600

    add some functionality

--- src/App.js
+++ src/App.js
@@ -25,31 +25,35 @@ export default function App() {
 }
 
 function Accordion({ data }) {
+  const [curOpen, isCurOpen] = useState(null);
   return (
     <div className="accordion">
       {data.map((item, i) => (
         <AccordionItem
           key={item.num}
-          num={i + 1}
+          num={i}
           title={item.title}
-          text={item.text}
-        />
+          curOpen={curOpen}
+          onCurOpen={isCurOpen}
+        >
+          {item.text}
+        </AccordionItem>
       ))}
     </div>
   );
 }
 
-function AccordionItem({ num, title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
+function AccordionItem({ num, title, curOpen, onCurOpen, children }) {
+  const isOpen = num === curOpen;
   function handleToggle() {
-    setIsOpen((isOpen) => !isOpen);
+    onCurOpen(isOpen ? null : num);
   }
   return (
     <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
-      <p className="number">{num > 9 ? num : `0${num}`}</p>
+      <p className="number">{num > 9 ? num + 1 : `0${num + 1}`}</p>
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
-      {isOpen && <div className="content-box">{text}</div>}
+      {isOpen && <div className="content-box">{children}</div>}
     </div>
   );
 }
